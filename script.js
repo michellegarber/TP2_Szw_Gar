37c2b94f7fc9ea4bdfe0a29a43783b46edfbc4da
MICHELLE SOFIA GARBER <48521612@ort>
2025-03-31 15:57:35 -0300

    frff

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-var ListaTareas=[];
-let tarea = document.getElementById("tarea");
+var ListaTareas = [];
 
-function AgregarTarea(){
-    ListaTareas.push("tarea");
+const tareaInput = document.getElementById("tarea");
+const lista = document.getElementById('tareaElemento')
+const listaTareasElement = document.getElementById("listaTareas");
+const formularioTareas = document.getElementById('formularioTareas');  
+const butonEnviar = document.getElementById('agregar')
+
+formularioTareas.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    
+    const input = tareaInput.value;
+
+    const agregarLI = document.createElement('li');
+    agregarLI.innerHTML = input;
+    
+    lista.appendChild(agregarLI);
+    
+    console.log(input); 
+    tareaInput.value = '';  
+});
+
+
+
+function ActualizarListaTareas() {
+    listaTareasElement.innerHTML = "";
+
+    for (let i = 0; i < ListaTareas.length; i++) {
+        let tareaElemento = document.createElement("li");
+        tareaElemento.textContent = ListaTareas[i];
+        listaTareasElement.appendChild(tareaElemento);
+    }
 }
